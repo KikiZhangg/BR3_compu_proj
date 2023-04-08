42933b9848b4359373b19a60ac6d3ca7b2342319
Kiki Zhang <szhan128@jhu.edu>
2023-04-08 19:34:28 -0400

    feature selection

---
+++ feature_selection.R
@@ -0,0 +1,45 @@
+set.seed(12345)
+library(e1071)
+source('msvmRFE.R')
+BR3_data <- read.csv('BR3_data.csv')
+BR3_meta <- read.csv('BR3_meta.csv')
+BR3 <- BR3_data[-c(1)] 
+label <- BR3_meta[ , ncol(BR3_meta), drop = FALSE]
+ChemoResponse <- ifelse(label$ChemoResponse == "Sensitive",1,0)
+BR3 <- cbind(ChemoResponse, BR3)
+
+# Take a look at the expected input structure
+dim(BR3)
+BR3[1:5,1:5]
+
+# Basic usage
+svmRFE(BR3, k=10, halve.above=500)
+
+# Set up cross validation
+nfold = 10
+nrows = nrow(input)
+folds = rep(1:nfold, len=nrows)[sample(nrows)]
+folds
+folds = lapply(1:nfold, function(x) which(folds == x))
+folds
+
+# Perform feature ranking on all training sets
+results = lapply(folds, svmRFE.wrap, input, k=10, halve.above=100)
+length(results)
+results
+
+# Obtain top features across ALL folds
+top.features = WriteFeatures(results, input, save=F)
+head(top.features)
+
+# Estimate generalization error using a varying number of top features
+featsweep = lapply(1:5, FeatSweep.wrap, results, input)
+featsweep
+
+# Make plot
+no.info = min(prop.table(table(input[,1])))
+errors = sapply(featsweep, function(x) ifelse(is.null(x), NA, x$error))
+
+dev.new(width=4, height=4, bg='white')
+PlotErrors(errors, no.info=no.info)
+dev.off()
